d289da2906d6ea2863234a7d6cf0cc30c003f0ee
Owataso <owatasosan001@gmail.com>
2021-02-23 15:58:24 +0900

    とりあえず必要そうなデータ系雑に定義

---
+++ Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Data
+{
+    /// <summary>
+    /// リプレイに必要なデータ周りの管理クラス(Singleton)
+    /// </summary>
+    public class DataManager
+    {
+        /// <summary>
+        /// 落としていくノーツのリスト
+        /// </summary>
+        public List<INote> NoteList;
+
+        /// <summary>
+        /// 自身の実体
+        /// </summary>
+        static DataManager instance;
+
+        /// <summary>
+        /// 実体のアクセサ
+        /// </summary>
+        public static DataManager Inst
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataManager();
+                }
+
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public DataManager()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Data/ReplayData.cs
@@ -0,0 +1,87 @@
+﻿namespace Data
+{
+    /// <summary>
+    /// キー入力種別
+    /// ここでは見た目上の観点からノーツと定義する
+    /// </summary>
+    public enum NoteType
+    {
+        /// <summary>通常ノーツ</summary>
+        Normal,
+
+        /// <summary>ロングノーツ(開始)</summary>
+        LongStart,
+
+        /// <summary>ロングノーツ(終了)</summary>
+        LongEnd
+    }
+
+    /// <summary>
+    /// キー入力情報インターフェース
+    /// </summary>
+    public interface INote
+    {
+        /// <summary>発火時間</summary>
+        int EventTime { get; }
+
+        /// <summary>レーン番号</summary>
+        int LaneIndex { get; }
+    }
+
+    /// <summary>
+    /// 通常ノーツ
+    /// </summary>
+    public class NormalNote : INote
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public NormalNote()
+        {
+        }
+
+        /// <summary>発火時間</summary>
+        public int EventTime { get; private set; }
+
+        /// <summary>レーン番号</summary>
+        public int LaneIndex { get; private set; }
+    }
+
+    /// <summary>
+    /// ロングノーツ(開始)
+    /// </summary>
+    public class LongNoteStart : INote
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public LongNoteStart()
+        {
+        }
+
+        /// <summary>発火時間</summary>
+        public int EventTime { get; private set; }
+
+        /// <summary>レーン番号</summary>
+        public int LaneIndex { get; private set; }
+    }
+
+    /// <summary>
+    /// ロングノーツ(終了)
+    /// </summary>
+    public class LongNoteEnd : INote
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public LongNoteEnd()
+        {
+        }
+
+        /// <summary>発火時間</summary>
+        public int EventTime { get; private set; }
+
+        /// <summary>レーン番号</summary>
+        public int LaneIndex { get; private set; }
+    }
+}
